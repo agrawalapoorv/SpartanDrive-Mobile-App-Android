6c7c6e52889566fbe92b851794e81be8feda23fa
Namitha Shetty <shettynamitha22@gmail.com>
2015-12-05 16:12:29 -0800

    UI fixes

--- Namitha/SpartanDrive/app/src/main/java/cmpe277/sjsu/spartandrive/listContentsActivity.java
+++ Namitha/SpartanDrive/app/src/main/java/cmpe277/sjsu/spartandrive/listContentsActivity.java
@@ -3,6 +3,7 @@ package cmpe277.sjsu.spartandrive;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -15,6 +16,8 @@ import com.google.android.gms.drive.DriveContents;
 import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.DriveFolder;
 import android.util.Log;
+import android.widget.PopupMenu;
+
 /**
  * Created by namithashetty on 11/26/15.
  */
@@ -24,6 +27,8 @@ public class listContentsActivity extends ConnectionActivity {
     private ViewAdapter mResultsAdapter;
     private GridView mListViewSamples;
     private static final String TAG = "ConnectionDriveActivity";
+    private String driveId;
+
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -33,14 +38,20 @@ public class listContentsActivity extends ConnectionActivity {
         mResultsAdapter = new ViewAdapter(this);
         mResultsListView.setAdapter(mResultsAdapter);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.hide();
+
+        FloatingActionButton search = (FloatingActionButton) findViewById(R.id.search_button);
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(),  CustomFolderNameActivity.class);
+                //EditText searchText=(EditText)findViewById(R.id.searchText);
+                //searchTerm = searchText.getText().toString();
+                Intent intent = new Intent(getBaseContext(), SearchPopUpActivity.class);
+                //intent.putExtra("searchTerm", searchTerm);
                 startActivity(intent);
             }
-        });*/
+        });
 
         //Code to handle onClick for CreateFolder button
        /* final Button button = (Button) findViewById(R.id.fab);
@@ -52,15 +63,44 @@ public class listContentsActivity extends ConnectionActivity {
             }
         });
 */
-        /*mListViewSamples = (ListView) findViewById(R.id.listItems);
+        mListViewSamples = (GridView) findViewById(R.id.listItems);
         mListViewSamples.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int i, long arg3) {
-                Drive.DriveApi.fetchDriveId(getGoogleApiClient(), "0B8m6vocGpnl3TVh6bnJ0eUxJYlU")
-                      .setResultCallback(idCallback);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(TAG, "Selected item:" + mResultsAdapter.getItem(position).getDriveId());
+                driveId = mResultsAdapter.getItem(position).getDriveId().encodeToString();
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(listContentsActivity.this, mListViewSamples);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater()
+                        .inflate(R.menu.menu_main, popup.getMenu());
 
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Log.i(TAG, "res" + item.getTitle());
+                        if (item.getTitle().equals("View Content")) {
+                            Intent intent = new Intent(getBaseContext(), listContentsActivity.class);
+                            intent.putExtra("driveId", driveId);
+                            startActivity(intent);
+                        }
+                        if (item.getTitle().equals("Edit")) {
+                            Intent intent = new Intent(getBaseContext(), EditFolderActivity.class);
+                            intent.putExtra("driveId", driveId);
+                            startActivity(intent);
+                        }
+                        if (item.getTitle().equals("Delete")) {
+                            Intent intent = new Intent(getBaseContext(), DeleteFolderActivity.class);
+                            intent.putExtra("driveId", driveId);
+                            startActivity(intent);
+                        }
+                        return true;
+                    }
+                });
+
+                popup.show(); //showing popup menu
             }
-        });*/
+        });
     }
 
     @Override
@@ -73,55 +113,7 @@ public class listContentsActivity extends ConnectionActivity {
         DriveFolder folder = drive.asDriveFolder();
         folder.listChildren(getGoogleApiClient())
                 .setResultCallback(metadataResult);
-        //Drive.DriveApi.fetchDriveId(getGoogleApiClient(), "0B8m6vocGpnl3VGVzelctREUxYlk")
-          //      .setResultCallback(idCallback);
     }
-
-    /*final private ResultCallback<DriveApi.DriveContentsResult> idCallback = new ResultCallback<DriveApi.DriveContentsResult>() {
-        @Override
-        public void onResult(DriveApi.DriveContentsResult result) {
-            if (!result.getStatus().isSuccess()) {
-                showMessage("Cannot find DriveId. Are you authorized to view this file?");
-                return;
-            }
-            DriveContents driveContent = result.getDriveContents();
-            Log.i(TAG, "Request code" + driveContent);
-
-            DriveId driveId = driveContent.getDriveId();
-            Log.i(TAG, "Request code" + driveId);
-            DriveFolder folder = driveId.asDriveFolder();
-            folder.listChildren(getGoogleApiClient())
-                    .setResultCallback(metadataResult);
-        }
-    };
-
-    final private ResultCallback<DriveApi.MetadataBufferResult> metadataResult = new
-            ResultCallback<DriveApi.MetadataBufferResult>() {
-                @Override
-                public void onResult(DriveApi.MetadataBufferResult result) {
-                    if (!result.getStatus().isSuccess()) {
-                        showMessage("Problem while retrieving files");
-                        return;
-                    }
-                    mResultsAdapter.clear();
-                    mResultsAdapter.append(result.getMetadataBuffer());
-                    showMessage("Successfully listed files.");
-                }
-            };*/
-
-    /*final private ResultCallback<DriveApi.DriveIdResult> idCallback = new ResultCallback<DriveApi.DriveIdResult>() {
-        @Override
-        public void onResult(DriveApi.DriveIdResult result) {
-            if (!result.getStatus().isSuccess()) {
-                showMessage("Cannot find DriveId. Are you authorized to view this file?");
-                return;
-            }
-            DriveId driveId = result.getDriveId();
-            DriveFolder folder = driveId.asDriveFolder();
-            folder.listChildren(getGoogleApiClient())
-                    .setResultCallback(metadataResult);
-        }
-    };*/
 
     final private ResultCallback<DriveApi.MetadataBufferResult> metadataResult = new
             ResultCallback<DriveApi.MetadataBufferResult>() {
